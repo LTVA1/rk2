40a739514107162d18ac64abe818b5732276a074
LTVA1 <87536432+LTVA1@users.noreply.github.com>
2022-05-15 18:28:02 +0300

    st

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <QCoreApplication>
+#include <iostream>
+#include "tasks_rk2.h"
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication a(argc, argv);
+
+	//task 2
+	int test[11] = {1, 3, 5, 6, 7, 10, 20, 69, 420, 666, 667};
+
+	std::pair<int, int> one = binSearch(test, 11, 3);
+	std::pair<int, int> two = binSearch(test, 11, 420);
+
+	std::cout << one.first << ";" << one.second << " " << two.first << ";" << two.second << "\n\n";
+
+	//task 4
+
+	std::pair<int, double> test1 = std::make_pair(10, 40.67565);
+	std::cout << test1 << "\n";
+
+	//task 5
+	const char* right_brackets = "{()}";
+	const char* unholy_brackets = "{(})[{]}";
+
+	CheckBrackets<char> br;
+
+	std::cout << "The string is all good, so here should be 1: " << br.checkBrackets(right_brackets) << ", but this string is wrong so here should be -1: " << br.checkBrackets(unholy_brackets) << "\n\n";
+
+	return a.exec();
+}
---
+++ tasks_rk2.h
@@ -0,0 +1,132 @@
+#include <list>
+#include <vector>
+#include <iostream>
+
+struct Node
+{
+	Node();
+	Node(int nameNode);
+	~Node();
+
+	Node* parent;
+	std::list<Node*> listChilds;
+	int name;
+	static int countNodes;
+};
+
+class Graph
+{
+	private:
+		Node* head;
+		//здесь можно писать любые функции, которые могут понадобиться
+
+	public:
+		Graph();
+		Graph(int countNodes);
+		~Graph();
+
+		/*
+				output		:	-1 - функция не реализована
+				author		:
+				date		:
+			*/
+		int buildTreeBFS(int countNodes);
+
+
+		/*
+				output		:
+							:	положительное число -- число узлов
+				author		:
+				date		:
+			*/
+		int buildTreeDFS(int countNodes);
+
+		void BFS();
+		void DFS();
+
+
+		/*
+				description	:	функция поиска узла по его имени
+				input 		:	nameNode -- имя узла
+				output		:	{bool -- узел найден, list<int> -- список имён узлов до необходимого}
+				description	:	приставка DFS -- поиск в глубину, BFS -- поиск в ширину
+				author		:
+				date		:
+			*/
+		std::pair<bool, std::list<int>> searchDFS(int nameNode);
+		std::pair<bool, std::list<int>> searchBFS(int nameNode);
+};
+
+std::pair<int/*index el*/,int /*count call*/> binSearch(int* ar, int sizeAr, int el);
+
+template<class T>
+class Filo
+{
+	private:
+		T* ar;	//массив
+		int size;	//размер массива
+		int countData;	//число элементов в массиве
+
+	public:
+		Filo()
+		{
+			size = 0;
+			countData = 0;
+			ar = nullptr;
+		}
+
+		Filo(int _size)
+		{
+			size = _size;
+			ar = new T(size);
+			countData = 0;
+		}
+
+		~Filo()
+		{
+			if(ar != nullptr)
+			{
+				delete[] ar;
+			}
+		}
+		/*
+				input		:	элемент который надо добавить
+				output		:	1 - элемент добавлен, 0 - элемент не добавлен
+				description	:	добавление элемента в конец массива
+			*/
+		int push_back(T el);
+		/*
+				input		:	элемент который надо извлечь
+				output		:	пара значений:
+									1ый аргумент пары -- 1 - элемент успешно извлечён, 0 - элементов в массиве нет
+									2ой аргумент пары -- это извлечённый элемент
+				description	:	извлечение элемента из массива
+			*/
+		std::pair<int,T> pop_back();
+};
+
+template <typename T>
+std::ostream& operator<< (std::ostream& output, const std::pair<int, T>& p)
+{
+	output << "(" << p.first << ", " << p.second << ")\n";
+	return output;
+}
+
+template<class T>
+class CheckBrackets : public Filo<char>
+{
+	private:
+		//можно объявлять любые переменные и функции
+		char find_pair(char c);
+		bool is_open_bracket(char c);
+	public :
+		CheckBrackets() : Filo<char>(3000000) { }
+		~CheckBrackets() {}
+
+		/*
+			input		:	строка со скобками
+			output		:	0 - всё хорошо, -1 - скобок не хватает
+			description	:	проверка скобок,
+		*/
+		int checkBrackets(const char* strBrackets);
+};
